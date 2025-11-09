ff72c1d97e771c8360e2b0647adeddb384b5038f
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-11-09 19:59:26 +0530

    FIX: route reload issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages({ cname: 'bramhadeshmukh.me' })],
-  // Use root base for custom domain or user/organization pages
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
+  plugins: [react()],
+  base: '/' // or '/your-repo-name/' on GitHub Pages
 });
